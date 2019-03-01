d548fb90624906cf4f5dccd1e8f6bffd61cb206b
Alex KM <habsters@hotmail.com>
2019-03-01 14:22:48 +0800

    opening ceremony

---
+++ Trippie/app/src/main/java/com/example/eunicechu/trippie/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.eunicechu.trippie;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void activityMap (View v)
+    {
+        Intent i = new Intent(this, MapActivity.class);
+        startActivity(i);
+    }
+
+}
